893bdfc988b209219a87eb5296d5a14324e65e6a
InfiTualEr <InfiTualEr@protonmail.com>
2020-02-23 03:27:57 +0330

    done for today

---
+++ precache-manifest.458bfa268cafcdca990e589b36cbdc0b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03780e8a55f061664a0",
+    "url": "/candlestick/css/app.a51024df.css"
+  },
+  {
+    "revision": "2b0e60c6286ce370e725",
+    "url": "/candlestick/css/patterns.0a3cc36c.css"
+  },
+  {
+    "revision": "06d42e475259e543806e34cde62f444e",
+    "url": "/candlestick/fonts/Vazir-Bold.06d42e47.woff"
+  },
+  {
+    "revision": "6c0a5ff35524caf9299877ad884e4733",
+    "url": "/candlestick/fonts/Vazir-Light.6c0a5ff3.woff"
+  },
+  {
+    "revision": "f2bd4a28e444d8f2ac28e9ce07b486b2",
+    "url": "/candlestick/fonts/Vazir-Medium.f2bd4a28.woff"
+  },
+  {
+    "revision": "331c39315fd661b27d8bba271fdb3fc0",
+    "url": "/candlestick/fonts/Vazir.331c3931.woff"
+  },
+  {
+    "revision": "9bcfc918c4594e0dd689e32b1c42bac4",
+    "url": "/candlestick/img/Hammer1.9bcfc918.png"
+  },
+  {
+    "revision": "cc47179f9ff2592b06ca6caebddda6a2",
+    "url": "/candlestick/index.html"
+  },
+  {
+    "revision": "f03780e8a55f061664a0",
+    "url": "/candlestick/js/app.b3f9cae1.js"
+  },
+  {
+    "revision": "fa1072aa60a2bd087203",
+    "url": "/candlestick/js/chunk-vendors.ae311cfd.js"
+  },
+  {
+    "revision": "2b0e60c6286ce370e725",
+    "url": "/candlestick/js/patterns.775fdb8f.js"
+  },
+  {
+    "revision": "984eed04becd8e8d4bfd3e1e893a5d45",
+    "url": "/candlestick/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/candlestick/robots.txt"
+  }
+]);
